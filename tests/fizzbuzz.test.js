9fd2c1f4184583f5154cee9816b12e87621779fa
chetta chetta chetta <>
2019-04-06 12:39:35 +0700

    verify 2

--- tests/fizzbuzz.test.js
+++ tests/fizzbuzz.test.js
@@ -3,3 +3,7 @@ const fizzbuzz = require('../src/fizzbuzz')
 test('should return 1 when enter 1', () => {
     expect(1).toBe(fizzbuzz(1));
 });
+
+test('should return 2 when enter 2', () => {
+    expect(2).toBe(fizzbuzz(2))
+})
