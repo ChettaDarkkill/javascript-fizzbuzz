583133e41879822811245ef9c625e766ad88974b
chetta chetta chetta <>
2019-04-06 13:47:43 +0700

    refacter test

--- tests/fizzbuzz.test.js
+++ tests/fizzbuzz.test.js
@@ -1,121 +1,41 @@
 const fizzbuzz = require('../src/fizzbuzz')
 
-test('should return 1 when enter 1', () => {
-    expect(1).toBe(fizzbuzz(1));
-});
+var arr = {
+    1 : 1,
+    2 : 2,
+    3 : "Fizz",
+    4 : 4,
+    5 : "Buzz",
+    6 : "Fizz",
+    7 : 7,
+    8 :  8,
+    9 :  "Fizz",
+    10 :  "Buzz",
+    11 :  11,
+    12 :  "Fizz",
+    13 :  13,
+    14 :  14,
+    15 :  "FizzBuzz",
+    16 :  16,
+    17 :  17,
+    18 :  "Fizz",
+    19 :  19,
+    20 :  "Buzz",
+    21 :  "Fizz",
+    22 :  22,
+    23 :  23,
+    24 :  "Fizz",
+    25 :  "Buzz",
+    26 :  26,
+    27 :  "Fizz",
+    28 :  28,
+    29 :  29,
+    30 :  "FizzBuzz"
+}
 
-test('should return 2 when enter 2', () => {
-    expect(2).toBe(fizzbuzz(2))
-})
+for(i in arr) { 
+    test(`should return ${arr[i]} when enter ${i}`, () => {
+        expect(fizzbuzz(i)).toBe(arr[i]);
+    });
+}
 
-test('should return Fizz when enter 3', () => {
-    expect(fizzbuzz(3)).toBe("Fizz")
-})
-
-test('should return 4 when enter 4', () => {
-    expect(4).toBe(fizzbuzz(4))
-})
-
-test('should return Buzz when enter 5', () => {
-    expect(fizzbuzz(5)).toBe("Buzz")
-})
-
-test('should return Fizz when enter 6', () => {
-    expect(fizzbuzz(6)).toBe("Fizz")
-})
-
-test('should return 7 when enter 7', () => {
-    expect(fizzbuzz(7)).toBe(7)
-})
-
-test('should return 8 when enter 8', () => {
-    expect(fizzbuzz(8)).toBe(8)
-})
-
-test('should return Fizz when enter 9', () => {
-    expect(fizzbuzz(9)).toBe("Fizz")
-})
-
-test('should return Buzz when enter 10', () => {
-    expect(fizzbuzz(10)).toBe("Buzz")
-})
-
-test('should return 11 when enter 11', () => {
-    expect(fizzbuzz(11)).toBe(11)
-})
-
-test('should return Fizz when enter 12', () => {
-    expect(fizzbuzz(12)).toBe("Fizz")
-})
-
-test('should return 13 when enter 13', () => {
-    expect(fizzbuzz(13)).toBe(13)
-})
-
-test('should return 14 when enter 14', () => {
-     expect(fizzbuzz(14)).toBe(14)
-})
-
-test('should return FizzBuzz when enter 15', () => {
-    expect(fizzbuzz(15)).toBe("FizzBuzz")
-})
-
-test('should return 16 when enter 16', () => {
-     expect(fizzbuzz(16)).toBe(16)
-})
-
-test('should return 17 when enter 17', () => {
-    expect(fizzbuzz(17)).toBe(17)
-})
-
-test('should return Fizz when enter 18', () => {
-    expect(fizzbuzz(18)).toBe("Fizz")
-})
-
-test('should return 19 when enter 19', () => {
-    expect(fizzbuzz(19)).toBe(19)
-})
-
-test('should return Buzz when enter 20', () => {
-    expect(fizzbuzz(20)).toBe("Buzz")
-})
-
-test('should return Fizz when enter 21', () => {
-    expect(fizzbuzz(21)).toBe("Fizz")
-})
-
-test('should return 22 when enter 22', () => {
-    expect(fizzbuzz(22)).toBe(22)
-})
-
-test('should return 23 when enter 23', () => {
-    expect(fizzbuzz(23)).toBe(23)
-})
-
-test('should return Fizz when enter 24', () => {
-    expect(fizzbuzz(24)).toBe("Fizz")
-})
-
-test('should return Buzz when enter 25', () => {
-    expect(fizzbuzz(25)).toBe("Buzz")
-})
-
-test('should return 26 when enter 26', () => {
-    expect(fizzbuzz(26)).toBe(26)
-})
-
-test('should return Fizz when enter 27', () => {
-    expect(fizzbuzz(27)).toBe("Fizz")
-})
-
-test('should return 28 when enter 28', () => {
-    expect(fizzbuzz(28)).toBe(28)
-})
-
-test('should return 29 when enter 29', () => {
-    expect(fizzbuzz(29)).toBe(29)
-})
-
-test('should return FizzBuzz when enter 30', () => {
-    expect(fizzbuzz(30)).toBe("FizzBuzz")
-})
